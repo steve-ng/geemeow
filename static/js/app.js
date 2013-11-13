cb449f3dbb5a40cb51302b3dc3979e219c4ba7d7
Steve Ng <steveng.1988@gmail.com>
2013-11-14 01:25:44 +0800

    Updated with 3 steps

--- static/js/app.js
+++ static/js/app.js
@@ -19,7 +19,6 @@ $(document).ready(function () {
     //prevent clicking to highlight
     $('#cat_holder').mousedown(function(){ return false; })
     $('#little_cat_container').mousedown(function(){ return false; })
-    $('.home-section').mousedown(function(){ return false; })
 
     var animation = $('.tuna_container');
 
