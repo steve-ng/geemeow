a3165027dc664fdc8392de1400976055e22699d4
hendychua <hendychua@gmail.com>
2013-11-13 15:15:32 +0800

    added ssl for peerjs-server

--- servers/peerjs_server/server.js
+++ servers/peerjs_server/server.js
@@ -1,4 +1,9 @@
+var fs = require('fs');
 var PeerServer = require('peer').PeerServer;
 var server = new PeerServer({
-    port: 3216
+    port: 3216,
+    ssl: {
+        key: fs.readFileSync('/etc/ssl/private/privatekey.pem'),
+        certificate: fs.readFileSync('/etc/ssl/certs/server.crt')
+    }
 });
