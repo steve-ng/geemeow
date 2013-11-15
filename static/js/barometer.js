dfe097673abf087802d546e0d14cd59e07b7c6d7
Steve Ng <steveng.1988@gmail.com>
2013-11-15 10:40:02 +0800

    Updated feedback tab, added back geecat tab

--- static/js/barometer.js
+++ static/js/barometer.js
@@ -8,7 +8,8 @@ BAROMETER.load = function(barometer_id) {
   this.empty_url = "https://getbarometer.s3.amazonaws.com/assets/barometer/images/transparent.gif";
   this.feedback_url = 'http://getbarometer.com/system/feedback_form/' + this.barometer_id;
 
-  this.tab_html = '<a id="barometer_tab" onclick="BAROMETER.show();" href="#">Feedback</a>';
+  //this.tab_html = '<a id="barometer_tab" onclick="BAROMETER.show();" href="#">Feedback</a>';
+  this.tab_html = '<a id="barometer_tab" href="mailto:contact@geemeow.com"> Feedback </a>';
   this.overlay_html = '<div id="barometer_overlay" style="display: none;">' +
 			'<div id="barometer_main" style="top: 130.95px;">' +
      '<a id="barometer_close" onclick="document.getElementById(\'barometer_overlay\').style.display = \'none\';return false" href="#"/></a>' +
@@ -20,8 +21,6 @@ BAROMETER.load = function(barometer_id) {
 				'</div>' +
 	'</div>';
 
-
-  
        
     document.write(this.tab_html);
     document.write(this.overlay_html);    
