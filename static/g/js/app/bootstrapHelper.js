56e5badb3d78e803ef6f1047f88570c5453f06ee
khankuan <khankuan@gmail.com>
2013-11-18 17:28:05 +0800

    more tooltips

--- static/g/js/app/bootstrapHelper.js
+++ static/g/js/app/bootstrapHelper.js
@@ -3,4 +3,9 @@ $(document).ready(function(){
 	//	Tooltip
 	$('#board-sidebar').find('li').tooltip({placement: 'bottom'});
 	$('#room_tab').tooltip({placement: 'bottom'});
+	$('#room_sync').tooltip({placement: 'bottom'});
+	$('#room_video').tooltip({placement: 'bottom'});
+	$('#room_mic').tooltip({placement: 'bottom'});
+	$('#room_setting').tooltip({placement: 'bottom'});
+
 });
