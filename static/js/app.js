0b01c0345fbe6054ba4042e71d1238200f4082bf
Steve Ng <steveng.1988@gmail.com>
2013-11-19 10:09:05 +0800

    Hidden non-essential element at landing page for mobile

--- static/js/app.js
+++ static/js/app.js
@@ -1,49 +1,64 @@
 $(document).ready(function () {
 
 
-
-	$('#for_geek').popover({
-		title: "",
-		html:"true",
-		content:"GeeMeow has an experimental version that uses entirely webRTC technology, no data transfer through a server! </br></br>"+
-				"<button id=\"geek_redirect\" class=\"btn popover_btn\">Try now! (Chrome only)  </button> &nbsp &nbsp" 
-	});
-
-	$('#for_geek').on('shown.bs.popover', function () {
-  		
-
-		$('#geek_redirect').click(function(e){
-
-		window.location.replace("/g/experimental.html");
-		})
-
-		$('#geek_close_btn').click(function(e){
-			$('#for_geek').popover('hide');
-		})
-	})
-
-
-	$('#try_now_btn').on('click',function(e){
-
-		var room_name_selected = $('#room_name').val();
-		window.location.replace("/g#"+room_name_selected);
-
-	})
-
 	if(document.body.style.MozTransform!=undefined)  {
 		console.log('firefox!');
-	  $('#cat_holder').hide();
+		$('#cat_holder').hide();
 	}
 
 
-	$("#takeALookBtn").click(function(){
-		$(".main").jumpTo($(this).data("target"));
-	});
+	var userAgent = navigator.userAgent.toLowerCase();
 
-	
-    $(".main").onepage_scroll({
-        sectionContainer: "section"
-    });
+	if ((userAgent.search("android|chrome") > -1) && (userAgent.search("mobile") > -1)){
+		hideNonEssentialElement();
+	}else if ((userAgent.search("iphone") > -1)){
+		hideNonEssentialElement();
+	}
+
+        //TODO, check for other type of mobile 
+
+
+        function hideNonEssentialElement(){
+        	$('#cat_holder').hide();
+        	$('#for_geek_info').hide();
+        }
+
+        $('#for_geek').popover({
+        	title: "",
+        	html:"true",
+        	content:"GeeMeow has an experimental version that uses entirely webRTC technology, no data transfer through a server! </br></br>"+
+        	"<button id=\"geek_redirect\" class=\"btn popover_btn\">Try now! (Chrome only)  </button> &nbsp &nbsp" 
+        });
+
+        $('#for_geek').on('shown.bs.popover', function () {
+
+
+        	$('#geek_redirect').click(function(e){
+
+        		window.location.replace("/g/experimental.html");
+        	})
+
+        	$('#geek_close_btn').click(function(e){
+        		$('#for_geek').popover('hide');
+        	})
+        })
+
+
+        $('#try_now_btn').on('click',function(e){
+
+        	var room_name_selected = $('#room_name').val();
+        	window.location.replace("/g#"+room_name_selected);
+
+        })
+
+        $("#takeALookBtn").click(function(){
+        	$(".main").jumpTo($(this).data("target"));
+        });
+
+
+        $(".main").onepage_scroll({
+        	sectionContainer: "section"
+        });
 
 
     //prevent clicking to highlight
@@ -53,94 +68,94 @@ $(document).ready(function () {
     var animation = $('.tuna_container');
 
     animation.one('webkitAnimationEnd oanimationend msAnimationEnd animationend',   
-    function(e) {
-	    
-	    $('#little_cat').removeClass('tuna-walk');
-	    $('#little_cat').addClass('tuna-sit');
+    	function(e) {
 
-	    createKittyPopOver();
-	    
-    });
+    		$('#little_cat').removeClass('tuna-walk');
+    		$('#little_cat').addClass('tuna-sit');
+
+    		createKittyPopOver();
+
+    	});
     var popoverContent;
 
-	function getPopOverContent(){
-		return popoverContent;
-	}
+    function getPopOverContent(){
+    	return popoverContent;
+    }
 
-	function createKittyPopOver(){
+    function createKittyPopOver(){
 
-		popoverContent =  "<div class=\"popover_thing\"> "+
-    						"I've been a naughty kitten, play a game of <b> Hit the Meow? </b>  " +
-	    					"Gameplay: Try to hit me while I'm on the run :P   " +
-	    					"<div class=\"kitten_conv\">" + 
-	    					"<button id=\"tuna_challenge_yes\" class=\"btn popover_btn\">Yes </button> &nbsp&nbsp"+
-	    					"<button id=\"tuna_challenge_no\" class=\"btn btn-danger\">Close</button>" +
-	    					"</div> " + 
-	    					"</div>";
+    	popoverContent =  "<div class=\"popover_thing\"> "+
+    	"I've been a naughty kitten, play a game of <b> Hit the Meow? </b>  " +
+    	"Gameplay: Try to hit me while I'm on the run :P   " +
+    	"<div class=\"kitten_conv\">" + 
+    	"<button id=\"tuna_challenge_yes\" class=\"btn popover_btn\">Yes </button> &nbsp&nbsp"+
+    	"<button id=\"tuna_challenge_no\" class=\"btn btn-danger\">Close</button>" +
+    	"</div> " + 
+    	"</div>";
 
-	    $('#little_cat').popover({
-	    	html:true,
-	    	placement:'top',
-	    	content:function () { return getPopOverContent(); },
-	    	delay:'0',
-	    	trigger:'manual'
-	    });
+    	$('#little_cat').popover({
+    		html:true,
+    		placement:'top',
+    		content:function () { return getPopOverContent(); },
+    		delay:'0',
+    		trigger:'manual'
+    	});
 
-	    $('#little_cat').popover('show');
-
-
-	    $('#tuna_challenge_yes').click(function(event){
-	    	$('#little_cat').popover('hide');
-	    	startGame();
-	    });
+    	$('#little_cat').popover('show');
 
 
-	    $('#tuna_challenge_no').click(function(event){
-			$('#little_cat').bind("click",createKittyPopOver);
-	    	$('#little_cat').popover('hide');
-	    	$('#timer_score').hide();
-	    });
-	}
-
-	var gameInterval; 
-	var score =0;
-
-	function startGame(){
+    	$('#tuna_challenge_yes').click(function(event){
+    		$('#little_cat').popover('hide');
+    		startGame();
+    	});
 
 
-		$('#little_cat').unbind("click",createKittyPopOver);
+    	$('#tuna_challenge_no').click(function(event){
+    		$('#little_cat').bind("click",createKittyPopOver);
+    		$('#little_cat').popover('hide');
+    		$('#timer_score').hide();
+    	});
+    }
+
+    var gameInterval; 
+    var score =0;
+
+    function startGame(){
+
+
+    	$('#little_cat').unbind("click",createKittyPopOver);
 
 		//start the cat animation to run
-	    $('#little_cat_container').removeClass('tuna_container');
-	    $('#little_cat_container').addClass("tuna_run_container");
+		$('#little_cat_container').removeClass('tuna_container');
+		$('#little_cat_container').addClass("tuna_run_container");
 
-	    $('#timer_score').show();
+		$('#timer_score').show();
 
-	    var timeleft = 10;
-	    $('#timer_left').text(timeleft);
+		var timeleft = 10;
+		$('#timer_left').text(timeleft);
 
-	    var x = 0;
-	    $('#little_cat').removeClass('tuna-sit');
-	    gameInterval = window.setInterval(function(){
-	    	if(x ==0){
-	    		x=1;	
-	    		$('#little_cat').removeClass('tuna-run-right');
-	    		$('#little_cat').addClass('tuna-run-left');		
-	    	}else{
-	    		x=0;
-	    		$('#little_cat').removeClass('tuna-run-left');
-	    		$('#little_cat').addClass('tuna-run-right');
-	    		timeleft--;
-	    		$('#timer_left').text(timeleft);
-	    	}
-	    	
-	    },500);
+		var x = 0;
+		$('#little_cat').removeClass('tuna-sit');
+		gameInterval = window.setInterval(function(){
+			if(x ==0){
+				x=1;	
+				$('#little_cat').removeClass('tuna-run-right');
+				$('#little_cat').addClass('tuna-run-left');		
+			}else{
+				x=0;
+				$('#little_cat').removeClass('tuna-run-left');
+				$('#little_cat').addClass('tuna-run-right');
+				timeleft--;
+				$('#timer_left').text(timeleft);
+			}
 
-
-	    $('#little_cat').bind("click",updateScoreAndPulsate);
+		},500);
 
 
-	    window.setTimeout(stopGame,10000);
+		$('#little_cat').bind("click",updateScoreAndPulsate);
+
+
+		window.setTimeout(stopGame,10000);
 	    //$('#little_cat').addClass('tuna-run');
 
 		//register a click counter that add score
@@ -150,15 +165,15 @@ $(document).ready(function () {
 
 	function updateScoreAndPulsate(){
 
-	    score++;
-	    $('#score_result').text(score);
-	    $( "#little_cat" ).effect('pulsate',{times:1},50,function(){});
+		score++;
+		$('#score_result').text(score);
+		$( "#little_cat" ).effect('pulsate',{times:1},50,function(){});
 	}
 
 	function stopGame(){
 
-	    $('#little_cat').unbind("click",updateScoreAndPulsate);
-	    $('#timer_left').text('0');
+		$('#little_cat').unbind("click",updateScoreAndPulsate);
+		$('#timer_left').text('0');
 
 		clearInterval(gameInterval);
 
@@ -190,27 +205,27 @@ $(document).ready(function () {
 
 	function startAgain(){
 		popoverContent =  "<div class=\"popover_thing\"> "+
-								"Hit me? </br> </br>" +
-	    						"<div class=\"kitten_conv\">" + 
-	    						"<button id=\"tuna_challenge_yes\" class=\"btn popover_btn\">Yes </button> &nbsp&nbsp"+
-	    						"<button id=\"tuna_challenge_no\" class=\"btn btn-danger\">Close</button>" +
-	    						"</div>" +
-	    						"</div>" ;
+		"Hit me? </br> </br>" +
+		"<div class=\"kitten_conv\">" + 
+		"<button id=\"tuna_challenge_yes\" class=\"btn popover_btn\">Yes </button> &nbsp&nbsp"+
+		"<button id=\"tuna_challenge_no\" class=\"btn btn-danger\">Close</button>" +
+		"</div>" +
+		"</div>" ;
 
-	    $('#little_cat').popover('show');
-	    $('#timer_score').show();
+		$('#little_cat').popover('show');
+		$('#timer_score').show();
 
-	    $('#tuna_challenge_yes').click(function(event){
-	    	$('#little_cat').popover('hide');
-	    	score = 0;
-	    	startGame();
-	    });
+		$('#tuna_challenge_yes').click(function(event){
+			$('#little_cat').popover('hide');
+			score = 0;
+			startGame();
+		});
 
 
-	    $('#tuna_challenge_no').click(function(event){
-	    	$('#little_cat').popover('hide');
-	    	$('#timer_score').hide();
-	    });
+		$('#tuna_challenge_no').click(function(event){
+			$('#little_cat').popover('hide');
+			$('#timer_score').hide();
+		});
 	}
 
 	function createRestartPopOver(){
@@ -229,85 +244,85 @@ $(document).ready(function () {
 		}
 
 		popoverContent =  "<div class=\"popover_thing\"> "+
-								remark+ 
-								" Play again?" + 
-								"</br> </br>" +
-	    						"<div class=\"kitten_conv\">" + 
-	    						"<button id=\"tuna_challenge_yes\" class=\"btn popover_btn\">Yes </button> &nbsp&nbsp"+
-	    						"<button id=\"tuna_challenge_no\" class=\"btn btn-danger\">Close</button>" +
-	    						"</div>" +
-	    						"</div>" ;
+		remark+ 
+		" Play again?" + 
+		"</br> </br>" +
+		"<div class=\"kitten_conv\">" + 
+		"<button id=\"tuna_challenge_yes\" class=\"btn popover_btn\">Yes </button> &nbsp&nbsp"+
+		"<button id=\"tuna_challenge_no\" class=\"btn btn-danger\">Close</button>" +
+		"</div>" +
+		"</div>" ;
 
-	    $('#little_cat').popover('show');
+		$('#little_cat').popover('show');
 
-	    $('#tuna_challenge_yes').click(function(event){
-	    	$('#little_cat').popover('hide');
-	    	score = 0;
-	    	startGame();
-	    });
+		$('#tuna_challenge_yes').click(function(event){
+			$('#little_cat').popover('hide');
+			score = 0;
+			startGame();
+		});
 
 
-	    $('#tuna_challenge_no').click(function(event){
+		$('#tuna_challenge_no').click(function(event){
 			$('#little_cat').bind("click",startAgain);
-	    	$('#little_cat').popover('hide');
-	    	$('#timer_score').hide();
-	    });
+			$('#little_cat').popover('hide');
+			$('#timer_score').hide();
+		});
 	}
 
 	function touchHandler(event)
 	{
 	    //alert("handler");
 	    var touches = event.changedTouches,
-	        first = touches[0],
-	        type = "";
+	    first = touches[0],
+	    type = "";
 	    
 	    switch(event.type)
 	    {
-	        case "touchstart": type = "click"; break;
-	        case "touchmove":  type="mousemove"; break;        
-	        case "touchend":   type="mouseup"; break;
-	        default: return;
+	    	case "touchstart": type = "click"; break;
+	    	case "touchmove":  type="mousemove"; break;        
+	    	case "touchend":   type="mouseup"; break;
+	    	default: return;
 	    }
 
 	             //initMouseEvent(type, canBubble, cancelable, view, clickCount, 
 	    //           screenX, screenY, clientX, clientY, ctrlKey, 
 	    //           altKey, shiftKey, metaKey, button, relatedTarget);
-	    
-	    var simulatedEvent = document.createEvent("MouseEvent");
-	    simulatedEvent.initMouseEvent(type, true, true, window, 1, 
-	                              first.screenX, first.screenY, 
-	                              first.clientX, first.clientY, false, 
-	                              false, false, false, 0/*left*/, null);
 
-	    first.target.dispatchEvent(simulatedEvent);
-	    event.preventDefault();
+var simulatedEvent = document.createEvent("MouseEvent");
+simulatedEvent.initMouseEvent(type, true, true, window, 1, 
+	first.screenX, first.screenY, 
+	first.clientX, first.clientY, false, 
+	false, false, false, 0/*left*/, null);
+
+first.target.dispatchEvent(simulatedEvent);
+event.preventDefault();
+}
+
+function touchStartToClickHandler() 
+{
+	document.addEventListener("touchstart", touchHandler, true);
+	document.addEventListener("touchmove", touchHandler, true);
+	document.addEventListener("touchend", touchHandler, true);
+	document.addEventListener("touchcancel", touchHandler, true);    
+}
+
+touchStartToClickHandler();
+
+
+$('#slide_content').children().hide();
+$('#slide_content0').fadeIn();
+
+$("#slider1").responsiveSlides({
+	auto: false,
+	pager: true,
+	nav: true,
+	speed: 500,
+	maxwidth: 800,
+	namespace: "transparent-btns",
+	before: function(erg){
+		$('#slide_content').children().hide();
+		$('#slide_content'+erg).fadeIn();
+		console.log(erg);
 	}
-
-	function touchStartToClickHandler() 
-	{
-	    document.addEventListener("touchstart", touchHandler, true);
-	    document.addEventListener("touchmove", touchHandler, true);
-	    document.addEventListener("touchend", touchHandler, true);
-	    document.addEventListener("touchcancel", touchHandler, true);    
-	}
-
-	touchStartToClickHandler();
-
-
-     $('#slide_content').children().hide();
-     $('#slide_content0').fadeIn();
-
-     $("#slider1").responsiveSlides({
-        auto: false,
-        pager: true,
-        nav: true,
-        speed: 500,
-        maxwidth: 800,
-        namespace: "transparent-btns",
-        before: function(erg){
-        	$('#slide_content').children().hide();
-        	$('#slide_content'+erg).fadeIn();
-            console.log(erg);
-        }
-     });
+});
 });
