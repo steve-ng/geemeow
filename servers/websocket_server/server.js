0c7fda3c6d08ae65ee72e4ef3401da4a1d6db12b
khankuan <khankuan@gmail.com>
2013-12-17 16:49:06 +0800

    typo

--- servers/websocket_server/server.js
+++ servers/websocket_server/server.js
@@ -35,7 +35,7 @@ function checkServer(server){
 			almostDied = false;
 
 		//	Duration limit to 30 mins
-		if (new Date().getTime() - server.startedTime > 60*1000*30 
+		if (new Date().getTime() - server.startedTime > 60*1000*0.2 
 			&& server.getServerNodeId().toLowerCase().indexOf("geecat") != 0){
 			nodeStarServers[server.getServerNodeId()].stop();
 			delete nodeStarServers[server.getServerNodeId()];
